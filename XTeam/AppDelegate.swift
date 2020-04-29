bbfb549e46aa819e022b230edee4d03b01d7e11b
Justin Lennox <justinanthonylennox@gmail.com>
2020-04-29 14:58:34 -0400

    Initial commit + tile view

--- XTeam/AppDelegate.swift
+++ XTeam/AppDelegate.swift
@@ -11,10 +11,15 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let rootNavController = UINavigationController(rootViewController: ViewController())
+        rootNavController.isNavigationBarHidden = true
+        self.window?.rootViewController = rootNavController
+        self.window?.makeKeyAndVisible()
         return true
     }
 
